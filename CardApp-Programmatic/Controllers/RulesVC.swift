33564bdec4946f76f1a4de7e2be08ae86c00bc57
bahittinn <bahittin84@gmail.com>
2023-07-19 22:02:22 +0300

    Details VC

--- CardApp-Programmatic/Controllers/RulesVC.swift
+++ CardApp-Programmatic/Controllers/RulesVC.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class RulesVC: UIViewController {
 
-    let rulesLabel = UILabel()
+    let rulesLabel    = UILabel()
+    let titleLabel    = UILabel()
+    let exerciseLabel = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +21,8 @@ class RulesVC: UIViewController {
     
     func configureUI() {
         configureRulesLabel()
+        configureTitleLabel()
+        configureExerciseLabel()
     }
     func configureRulesLabel() {
         view.addSubview(rulesLabel)
@@ -33,5 +37,36 @@ class RulesVC: UIViewController {
             rulesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -30)
         ])
     }
+    
+    func configureTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "The value of each card represents the number of exercise you do. \n\nJ = 11, Q = 12, K = 13, A= 14"
+        titleLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        titleLabel.textAlignment = .center
+        titleLabel.lineBreakMode = .byWordWrapping
+        titleLabel.numberOfLines = 0
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: rulesLabel.bottomAnchor,constant: 25),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 30),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -30)
+        ])
+    }
+    
+    func configureExerciseLabel() {
+        view.addSubview(exerciseLabel)
+        exerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+        exerciseLabel.text = "♠️ =  Push-up \n\n❤️ =  Sit-up \n\n♣️ =  Burpees \n\n♦️ =  Jumping Jacks"
+        exerciseLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        exerciseLabel.numberOfLines = 0
+        
+        NSLayoutConstraint.activate([
+            exerciseLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor,constant: 25),
+            exerciseLabel.widthAnchor.constraint(equalToConstant: 200),
+            exerciseLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
  
 }
